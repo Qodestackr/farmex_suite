b533c2c24f9c5f6e95cce97d37f42463c928989a
Qodestackr <winchygichu@gmail.com>
2023-11-24 09:42:22 +0300

    Test an issue, drag.ts

--- js/drag.ts
+++ js/drag.ts
@@ -1,58 +1,3 @@
-// const Drag = (): void => {
-//   const draggables = document.querySelectorAll<HTMLElement>('.task');
-//   const droppables = document.querySelectorAll<HTMLElement>('.swim-lane');
-
-//   draggables.forEach((task) => {
-//     task.addEventListener('dragstart', () => {
-//       task.classList.add('is-dragging');
-//     });
-//     task.addEventListener('dragend', () => {
-//       task.classList.remove('is-dragging');
-//     });
-//   });
-
-//   droppables.forEach((zone) => {
-//     zone.addEventListener('dragover', (e) => {
-//       e.preventDefault();
-
-//       const bottomTask = insertAboveTask(zone, e.clientY);
-//       const curTask = document.querySelector('.is-dragging') as HTMLElement;
-
-//       if (!bottomTask) {
-//         zone.appendChild(curTask);
-//       } else {
-//         zone.insertBefore(curTask, bottomTask);
-//       }
-//     });
-//   });
-
-//   const insertAboveTask = (
-//     zone: HTMLElement,
-//     mouseY: number
-//   ): HTMLElement | null => {
-//     const els = zone.querySelectorAll<HTMLElement>('.task:not(.is-dragging)');
-
-//     let closestTask: HTMLElement | null = null;
-//     let closestOffset = Number.NEGATIVE_INFINITY;
-
-//     els.forEach((task) => {
-//       const { top } = task.getBoundingClientRect();
-
-//       const offset = mouseY - top;
-
-//       if (offset < 0 && offset > closestOffset) {
-//         closestOffset = offset;
-//         closestTask = task;
-//       }
-//     });
-
-//     return closestTask;
-//   };
-// };
-
-// export default Drag;
-
-
 const Drag = (): void => {
   const draggables = document.querySelectorAll<HTMLElement>('.task');
   const droppables = document.querySelectorAll<HTMLElement>('.swim-lane');
@@ -81,11 +26,10 @@ const Drag = (): void => {
     });
   });
 
-  // Intentional linting issue: Missing return type for the insertAboveTask function
   const insertAboveTask = (
     zone: HTMLElement,
     mouseY: number
-  ) => {
+  ): HTMLElement | null => {
     const els = zone.querySelectorAll<HTMLElement>('.task:not(.is-dragging)');
 
     let closestTask: HTMLElement | null = null;
@@ -102,8 +46,8 @@ const Drag = (): void => {
       }
     });
 
-    return closestTask;
+    // return closestTask;
   };
 };
 
-export default Drag;
+// export default Drag;
